27e1ca0585db328660d1e577ebe599b8b7a4224d
Jurdanas K. <jurdanas.kriauciunas@gmail.com>
2026-07-06 12:25:07 +0200

    [klv] scaffold marlin-klv crate

---
+++ crates/marlin-klv/src/lib.rs
@@ -0,0 +1,8 @@
+//! MISB ST 0601 (UAS Datalink Local Set) KLV encoder/decoder.
+#![doc(html_root_url = "https://docs.rs/marlin-klv/0.1.1")]
+#![no_std]
+
+extern crate alloc;
+
+#[cfg(test)]
+extern crate std;
